6573b4da24431eb7a2abc1b21067c27f39b11219
rinne9029 <0kt4x34y2ky637r@au.com>
2024-10-30 16:54:12 +0900

    とりあえずでプッシュ
    ボス敵でグリフォンを制作スタート
    これから制作を進めていきます。

---
+++ QuatrefoilMMO/Assets/RinneResource/Scripts/GryphonState/Gryphon_Battle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RinneResourceStateMachineAI
+{
+    public class Gryphon_Battle : State<EnemyAI>
+    {
+        public Gryphon_Battle(EnemyAI owner) : base(owner) {}
+
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Stay()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            
+        }
+    }
+}
+
